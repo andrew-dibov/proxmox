427fb4b16d01c0c3846b134309f305829bf3a743
Andrew Dibov <andrew.dibov@yandex.ru>
2026-09-25 13:55:22 +0900

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,28 @@ variable "pve__password" {
 
 # ---
 
+variable "pve__node_name" {
+  type = string
+  default = "proxmox"
+}
+
+variable "pve__datastore_id" {
+  type = string
+  default = "storage-zfs"
+}
+
+# ---
+
 terraform {
   required_version = ">= 1.16.0"
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
       version = "~> 0.106"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.6.1"
     }
   }
 }
@@ -31,4 +47,27 @@ provider "proxmox" {
   password = var.pve__password
 
   insecure = true
+
+  ssh {
+    agent    = false
+    username = "root"
+    private_key = tls_private_key.proxmox_ssh.private_key_openssh
+  }
+}
+
+# ---
+
+resource "tls_private_key" "proxmox_ssh" {
+  algorithm = "ED25519"
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.proxmox_ssh.private_key_openssh
+  filename        = "${path.module}/id_ed25519"
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.proxmox_ssh.public_key_openssh
+  filename = "${path.module}/id_ed25519.pub"
 }
